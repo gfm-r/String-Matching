2036473fc6aa903794eff4771dde603efb0aa20a
Unknown <pink.wolf@tuta.io>
2018-12-02 05:50:32 +0300

    add: KMP and Reading from file
    implement KMP algorithm and add the functionality of reading from file to feed the algorithm with data

--- main.c
+++ main.c
@@ -7,15 +7,13 @@
 #include <time.h>
 #include <string.h>
 
-KMPSearch(char* pat, char* txt);
-computeLPSArray(char* pat, int M, int lps[]);
 
 int main(int argc, char const *argv[]) {
 
         printf("\t '############ Starting Making The Fils ############\n" );
 
         /*srand = sets its argument seed as the seed for a new sequence of
-          pseudo-random numbers -> from man page*/
+           pseudo-random numbers -> from man page*/
         srand(time(NULL));
         char randChar='0';
 
@@ -28,7 +26,7 @@ int main(int argc, char const *argv[]) {
                 //sprintf will convert int to char in our case we want to convert the cunter number into digit to usit in the file name
                 sprintf(FileName,"%d",counter);
                 //add a '.txt' as an extention for every file we will created
-                strcat(FileName,".txt");
+                // strcat(FileName,".txt");
 
                 int file=creat(FileName,0666);
                 counter++;
@@ -42,73 +40,11 @@ int main(int argc, char const *argv[]) {
                         randChar=(rand()%(122-97))+97; //get new random char
                         write(file,&randChar,1);
                 }
-
+                char e=EOF;
+                write(file,&e,1);
                 counterForChar+=100;
                 close(file);//optinal: we can close the file that we created
         }
 
         return 0;
-}
-
-void KMPSearch(char* pat, char* txt){
-	int M = sizeof(pat);
-	int N = sizeof(txt);
-
-	// create lps[] that will hold the
-	// "longest prefix suffix" values for pattern
-	int lps[M];
-	int j = 0;
-
-	computeLPSArray(pat, M , lps);
-
-	int i = 0; // index for the text
-	while(i<N){
-		if(pat[j] == txt[i]){
-			j++;
-			i++;
-		}
-		if (j==M){
-			printf("index %d",i-j);
-			j=lps[j-1];
-		}// mismatch after j matches
-
-		else if(i<N && pat[j] != txt[i-j]){
-			// Do not match lps[0 to lps[j-1]] chars,
-		    // they will match anyway
-			if(j != 0){
-				j=lps[j-1];
-			}
-			else{
-				i=i+1;
-			}
-		}
-	}
-}
-void computeLPSArray(char* pat, int M, int lps[]){
-	// length of the previous longest prefix suffix
-	int l = 0;
-	int i = 1;
-	lps[0] = 0; //l[0] is always zero
-
-    // the loop calculates l[i] for i = 1 to M-1
-	while(i < M){
-		if(pat[i]==pat[l]){
-			l++;
-			lps[i]=1;
-			i++;
-		}
-		else{// (pat[i] != pat[l])
-       	 // This is an example.
-         // AAACAAAA and i = 7. The idea is the same to search step.
-			if(l != 0){
-				l = lps[l - 1];
-				// Also, note that we do not increment i here
-			}
-			else{
-				 // if l is zero
-				lps[i]=1;
-				i++;
-			}
-		}
-	}
 }
---
+++ main2.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/wait.h>
+
+void KMPSearch(char*,int,int*);
+void computeLPSArray(char* pat, int M, int* lps);
+
+int main(int argc, char *argv[])
+{
+        printf("## Welcome to the txt matcing program \n");
+        printf("##\tEnter the pattern that you want, note (10-char MAX)\n");
+        char pat[11] ={0};
+        fgets(pat,11,stdin);
+        printf("##\tEnter the files names -1 is for terminate\n");
+
+        int num=0;//the number of file
+        int files[99]={0};//arry for files names
+        int in=0;//variaple to save the file name from the user
+
+        scanf("%d",&in);//ask the user to enter firist file name
+        while (in!=-1) {//enter files name until we get -1 as terminated number
+                files[num]=in;
+                num++;
+                scanf("%d",&in);
+        }//for the while
+
+        KMPSearch(pat,num,files);
+        return 0;
+}
+
+void KMPSearch(char *pat,int num_of_File,int *files){
+        printf("\t\tKMPS Starting\n");
+        for (int f = 0; f < num_of_File; f++) {//run the KMPS for num_of_File that we enterd
+                int M = strlen(pat)-1;//the length of the pattern
+                int N = 0;//num of char in the file
+                char tmp_char;//we will use this char in difirent plases in the code
+                char file_name[3]={0};
+
+                sprintf(file_name,"%d",files[f]);//convert form int to char
+                // printf("##############################################\n" );
+                // printf("\t->opening file -> %s\n",file_name);
+
+                int file=open(file_name,O_RDONLY);
+                // printf("file ds =%d\n",file );//for test
+                read(file,&tmp_char,1);//one read to check,if the file is empty we will not inter the while
+                while (tmp_char!=EOF) {
+                        N++;
+                        read(file,&tmp_char,1);
+                }
+                // printf("\t->Number of char in the file is= %d\n", N);//FOR TEST
+                lseek(file,0,SEEK_SET);
+                // printf("pat char num= %d\n",M);//FOR TEST
+                // printf("%s\n",pat );
+                /////////////////////////////////////
+
+                // create lps[] that will hold the longest prefix suffix
+                // values for pattern
+                int lps[M];
+                // Preprocess the pattern (calculate lps[] array)
+                computeLPSArray(pat, M, lps);
+                int i = 0; // index for txt[]
+                int j = 0; // index for pat[]
+                int read_Char =read(file,&tmp_char,1);
+                if(read_Char<0) {
+                        perror("");
+                        exit(1);
+                }
+                i++;
+
+                while (i < N) {
+                        if (pat[j] == tmp_char) {
+                                read(file,&tmp_char,1);
+                                j++;
+                                i++;
+                        }
+
+                        if (j == M) {
+                                printf("##############################################\n" );
+                                printf("\t-->Found in file -> %s\n",file_name);
+                                printf("\t-->At index [%d] \n", (i - j));
+                                j = lps[j - 1];
+                        }
+                        // mismatch after j matches
+                        else if (i < N && pat[j] != tmp_char) {
+                                // Do not match lps[0..lps[j-1]] characters,
+                                // they will match anyway
+                                if (j != 0)
+                                        j = lps[j - 1];
+                                else{
+                                        read(file,&tmp_char,1);
+                                        i++;
+                                }
+                        }
+                }
+                close(file);
+        }
+}//for the function
+
+// Fills lps[] for given patttern pat[0..M-1]
+void computeLPSArray(char* pat, int M, int* lps)
+{
+        // length of the previous longest prefix suffix
+        int len = 0;
+        lps[0] = 0; // lps[0] is always 0
+        // the loop calculates lps[i] for i = 1 to M-1
+        int i = 1;
+        while (i < M) {
+                if (pat[i] == pat[len]) {
+                        len++;
+                        lps[i] = len;
+                        // printf("-------------match--------------------------\n" );//for test
+                        // printf("lps %d\n",lps[i] );//for test
+                        // printf("len %d\n",len );//for test
+                        i++;
+                }
+                else if (len != 0) {
+                        // len = lps[len - 1];//orignal code
+                        len = 0;//my code
+                        // printf("-------------not match--------------------------\n" );//for test
+                        // printf("lps %d\n",lps[i] );//for test
+                        // printf("len %d\n",len );//for test
+                        // Also, note that we do not increment
+                        // i here
+                }
+                else {        // if (len == 0)
+                        lps[i]=0;
+                        // printf("-------------len =0--------------------------\n" );//for test
+                        // printf("lps %d\n",lps[i]);//for test
+                        i++;
+                        // printf("len =%d\n",len );//for test
+                }
+        }
+        // printf("LPS=" );//for test
+        // int j;//for test
+        // for (j = 0; j < M-1; j++)//for test
+        //         printf("[%d],",lps[j]);//for test
+        // printf("[%d]\n",lps[j]);//for test
+}
